6d699100839b2c4ada1b56030d9bf4e87813615d
Roger <roger.wang.r@gmail.com>
2018-04-05 18:05:27 -0400

    add basic account creation

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default: ''
+  },
+  password: {
+    type: String,
+    default: ''
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
+});
+
+UserSchema.methods.generateHash = (password) => {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+}
+
+UserSchema.methods.validPassword = (password) => {
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ server/routes/api/signin.js
@@ -0,0 +1,86 @@
+const User = require('../../models/User');
+
+module.exports = (app) => {
+  // app.get('/api/counters', (req, res, next) => {
+  //   Counter.find()
+  //     .exec()
+  //     .then((counter) => res.json(counter))
+  //     .catch((err) => next(err));
+  // });
+  //
+  // app.post('/api/counters', function (req, res, next) {
+  //   const counter = new Counter();
+  //
+  //   counter.save()
+  //     .then(() => res.json(counter))
+  //     .catch((err) => next(err));
+  // });
+
+  /*
+   * Sign Up
+   */
+  app.post('/api/account/signup', (req, res, next) => {
+    const { body } = req;
+    const {
+      username,
+      email,
+      password
+    } = body;
+
+    if (!username) {
+      return res.send({
+        success: false,
+        message: 'Error: Username cannot be blank.'
+      });
+    }
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: Email address cannot be blank.'
+      });
+    }
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.'
+      });
+    }
+
+    email = email.toLowerCase();
+
+    User.find({
+      email: email
+    }), (err, previousUsers) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error.'
+        });
+      } else if (previousUsers.length > 0) {
+        return res.send({
+          success: false,
+          message: 'Error: Account already exists.'
+        });
+      }
+
+      const newUser = new User();
+      newUser.username = username;
+      newUser.email = email;
+      newUser.password = newUser.generateHash(password);
+
+      newUser.save((err, user) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: 'Error: Server error.'
+          });
+        }
+
+        return res.send({
+          success: true,
+          message: 'Account successfully created!'
+        })
+      })
+    }
+  });
+};
